305471ac32f7bff02bc38752d3e0bdbfe5908c24
QuocAnUIT <quocanuit@gmail.com>
2024-03-12 14:53:27 +0700

    Exercise 6

--- Lab1/Lab01-Bai06.Designer.cs
+++ Lab1/Lab01-Bai06.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Lab01_Bai06";
+            label1 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(155, 140);
+            label1.Name = "label1";
+            label1.Size = new Size(187, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Chọn ngày tháng năm sinh:";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(359, 140);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 1;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(359, 188);
+            label2.Name = "label2";
+            label2.Size = new Size(18, 20);
+            label2.TabIndex = 2;
+            label2.Text = "...";
+            // 
+            // Lab01_Bai06
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label1);
+            Name = "Lab01_Bai06";
+            Text = "Lab01_Bai06";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private DateTimePicker dateTimePicker1;
+        private Label label2;
     }
 }
