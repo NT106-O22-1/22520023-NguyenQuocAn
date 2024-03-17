abcd887b631938b8ddf693ca27a1c97e326d57b4
QuocAnUIT <quocanuit@gmail.com>
2024-03-17 09:50:42 +0700

    Exercise 8

--- Lab1/Lab01-Bai08.cs
+++ Lab1/Lab01-Bai08.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Lab1
 {
@@ -15,6 +16,39 @@ namespace Lab1
         public Lab01_Bai08()
         {
             InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            listView.Items.Add(new ListViewItem(inputTb.Text));
+            inputTb.Clear();
+        }
+
+        private void findBtn_Click(object sender, EventArgs e)
+        {
+            string[] meals = new string[listView.Items.Count];
+            int i = 0;
+            foreach (ListViewItem item in listView.Items)
+            {
+                meals[i] = item.Text;
+                i++;
+            }
+
+            Random random = new Random();
+            int r = random.Next(0, meals.Length);
+            outputTb.Text = meals[r];
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            inputTb.Clear();
+            listView.Clear();
+            outputTb.Clear();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
