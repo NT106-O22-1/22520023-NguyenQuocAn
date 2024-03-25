3de89550c3792e623a0250c27ca1546c57ed491a
QuocAnUIT <quocanuit@gmail.com>
2024-03-25 11:24:41 +0700

    Exercise 1

--- Lab2/Lab02_Bai01.cs
+++ Lab2/Lab02_Bai01.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Odbc;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,40 @@ namespace Lab2
         public Lab02_Bai01()
         {
             InitializeComponent();
+        }
+
+        private void readBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            if (!ofd.FileName.Contains("input1.txt"))
+            {
+                MessageBox.Show("Vui lòng tạo/chọn file input1.txt");
+                return;
+            }
+            FileStream fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate);
+            StreamReader sr = new StreamReader(fs);
+            richTextBox.AppendText(sr.ReadToEnd());
+
+            fs.Close();
+        }
+
+        private void writeBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            if (!ofd.FileName.Contains("output1.txt"))
+            {
+                MessageBox.Show("Vui lòng tạo/chọn file output1.txt");
+                return;
+            }
+            FileStream fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+            sw.Write(richTextBox.Text.ToUpper());
+            sw.Close();
+            MessageBox.Show("Đã ghi thành công vào output1.txt");
+
+            fs.Close();
         }
     }
 }
