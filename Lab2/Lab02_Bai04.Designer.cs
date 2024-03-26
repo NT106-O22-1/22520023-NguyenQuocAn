c3ff228fa919f30db09f8d019d3a2206f9a6286a
QuocAnUIT <quocanuit@gmail.com>
2024-03-26 16:11:23 +0700

    update

--- Lab2/Lab02_Bai04.Designer.cs
+++ Lab2/Lab02_Bai04.Designer.cs
@@ -28,12 +28,307 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pageNum = new System.Windows.Forms.Label();
+            this.oNameTxb = new System.Windows.Forms.TextBox();
+            this.oIdTxb = new System.Windows.Forms.TextBox();
+            this.oPhoneTxb = new System.Windows.Forms.TextBox();
+            this.oC1Txb = new System.Windows.Forms.TextBox();
+            this.oC2Txb = new System.Windows.Forms.TextBox();
+            this.oC3Txb = new System.Windows.Forms.TextBox();
+            this.oAvgTxb = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(205, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(236, 426);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(506, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(497, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(483, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Phone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(483, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Course 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(483, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Course 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(497, 294);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Course 3";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(497, 329);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 18);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Average";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(50, 88);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 18);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Name";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(50, 128);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 18);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "ID";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(34, 180);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 18);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Phone";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(34, 216);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(77, 18);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Course 1";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(34, 234);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(77, 18);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Course 2";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(25, 284);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(77, 18);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "Course 3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(486, 396);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(680, 387);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pageNum
+            // 
+            this.pageNum.AutoSize = true;
+            this.pageNum.Location = new System.Drawing.Point(610, 393);
+            this.pageNum.Name = "pageNum";
+            this.pageNum.Size = new System.Drawing.Size(14, 16);
+            this.pageNum.TabIndex = 16;
+            this.pageNum.Text = "0";
+            // 
+            // oNameTxb
+            // 
+            this.oNameTxb.Location = new System.Drawing.Point(643, 88);
+            this.oNameTxb.Name = "oNameTxb";
+            this.oNameTxb.Size = new System.Drawing.Size(100, 22);
+            this.oNameTxb.TabIndex = 17;
+            // 
+            // oIdTxb
+            // 
+            this.oIdTxb.Location = new System.Drawing.Point(599, 142);
+            this.oIdTxb.Name = "oIdTxb";
+            this.oIdTxb.Size = new System.Drawing.Size(100, 22);
+            this.oIdTxb.TabIndex = 18;
+            // 
+            // oPhoneTxb
+            // 
+            this.oPhoneTxb.Location = new System.Drawing.Point(613, 191);
+            this.oPhoneTxb.Name = "oPhoneTxb";
+            this.oPhoneTxb.Size = new System.Drawing.Size(100, 22);
+            this.oPhoneTxb.TabIndex = 19;
+            // 
+            // oC1Txb
+            // 
+            this.oC1Txb.Location = new System.Drawing.Point(613, 219);
+            this.oC1Txb.Name = "oC1Txb";
+            this.oC1Txb.Size = new System.Drawing.Size(100, 22);
+            this.oC1Txb.TabIndex = 20;
+            // 
+            // oC2Txb
+            // 
+            this.oC2Txb.Location = new System.Drawing.Point(613, 252);
+            this.oC2Txb.Name = "oC2Txb";
+            this.oC2Txb.Size = new System.Drawing.Size(100, 22);
+            this.oC2Txb.TabIndex = 21;
+            // 
+            // oC3Txb
+            // 
+            this.oC3Txb.Location = new System.Drawing.Point(613, 290);
+            this.oC3Txb.Name = "oC3Txb";
+            this.oC3Txb.Size = new System.Drawing.Size(100, 22);
+            this.oC3Txb.TabIndex = 22;
+            // 
+            // oAvgTxb
+            // 
+            this.oAvgTxb.Location = new System.Drawing.Point(599, 325);
+            this.oAvgTxb.Name = "oAvgTxb";
+            this.oAvgTxb.Size = new System.Drawing.Size(100, 22);
+            this.oAvgTxb.TabIndex = 23;
+            // 
+            // Lab02_Bai04
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.oAvgTxb);
+            this.Controls.Add(this.oC3Txb);
+            this.Controls.Add(this.oC2Txb);
+            this.Controls.Add(this.oC1Txb);
+            this.Controls.Add(this.oPhoneTxb);
+            this.Controls.Add(this.oIdTxb);
+            this.Controls.Add(this.oNameTxb);
+            this.Controls.Add(this.pageNum);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Lab02_Bai04";
             this.Text = "Lab02_Bai04";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label pageNum;
+        private System.Windows.Forms.TextBox oNameTxb;
+        private System.Windows.Forms.TextBox oIdTxb;
+        private System.Windows.Forms.TextBox oPhoneTxb;
+        private System.Windows.Forms.TextBox oC1Txb;
+        private System.Windows.Forms.TextBox oC2Txb;
+        private System.Windows.Forms.TextBox oC3Txb;
+        private System.Windows.Forms.TextBox oAvgTxb;
     }
 }
