9b1c87f663b7289ca6426256d552aaa89c115c86
Khang Nguyễn Đức <duckhang281004@gmail.com>
2024-04-18 12:11:41 +0700

    update lab3 bài 3

---
+++ Lab3_Bai03/Client.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Lab3_Bai03
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox_Message = new System.Windows.Forms.RichTextBox();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.button_Send = new System.Windows.Forms.Button();
+            this.button_Disconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox_Message
+            // 
+            this.richTextBox_Message.Location = new System.Drawing.Point(13, 104);
+            this.richTextBox_Message.Name = "richTextBox_Message";
+            this.richTextBox_Message.Size = new System.Drawing.Size(554, 168);
+            this.richTextBox_Message.TabIndex = 0;
+            this.richTextBox_Message.Text = "";
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(574, 104);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(162, 41);
+            this.button_Connect.TabIndex = 1;
+            this.button_Connect.Text = "Connect";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // button_Send
+            // 
+            this.button_Send.Location = new System.Drawing.Point(573, 170);
+            this.button_Send.Name = "button_Send";
+            this.button_Send.Size = new System.Drawing.Size(162, 41);
+            this.button_Send.TabIndex = 2;
+            this.button_Send.Text = "Send";
+            this.button_Send.UseVisualStyleBackColor = true;
+            this.button_Send.Click += new System.EventHandler(this.button_Send_Click);
+            // 
+            // button_Disconnect
+            // 
+            this.button_Disconnect.Location = new System.Drawing.Point(573, 231);
+            this.button_Disconnect.Name = "button_Disconnect";
+            this.button_Disconnect.Size = new System.Drawing.Size(162, 41);
+            this.button_Disconnect.TabIndex = 3;
+            this.button_Disconnect.Text = "Disconnect";
+            this.button_Disconnect.UseVisualStyleBackColor = true;
+            this.button_Disconnect.Click += new System.EventHandler(this.button_Disconnect_Click);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Disconnect);
+            this.Controls.Add(this.button_Send);
+            this.Controls.Add(this.button_Connect);
+            this.Controls.Add(this.richTextBox_Message);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox_Message;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.Button button_Send;
+        private System.Windows.Forms.Button button_Disconnect;
+    }
+}
---
+++ Lab3_Bai03/Server.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_Bai03
+{
+    public partial class Server : Form
+    {
+        private Socket socket;
+        private Socket client;
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Listen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Bind(new IPEndPoint(IPAddress.Any, 8080));
+                socket.Listen(5);
+                button_Listen.Enabled = false;
+                listView_Message.Items.Add("Đang lắng nghe...");
+
+                socket.BeginAccept(AcceptCallback, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                client = socket.EndAccept(ar);
+                listView_Message.Items.Add("Kết nối thành công!");
+
+                Thread thread = new Thread(HandleClient);
+                thread.Start();
+
+                socket.BeginAccept(AcceptCallback, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+        }
+
+        private void HandleClient()
+        {
+            while (true)
+            {
+                try
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[1024];
+                        int bytesReceive = client.Receive(buffer);
+                        string message = Encoding.UTF8.GetString(buffer, 0, bytesReceive);
+                        listView_Message.Invoke(new Action(() =>
+                        {
+                            ListViewItem item = new ListViewItem(message);
+                            listView_Message.Items.Add(item);
+                        }));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi: " + ex.Message);
+                    break;
+                }
+            }
+            client.Close();
+        }
+    }
+}
