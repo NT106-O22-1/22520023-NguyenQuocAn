63291a680746ce2b9315eb602926ffcfd6bb50c4
Khang Nguyễn Đức <duckhang281004@gmail.com>
2024-04-20 22:39:50 +0700

    update lab3 bài 4, bài 6

---
+++ Lab3_Bai04/Client.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using Newtonsoft.Json;
+
+namespace Lab3_Bai04
+{
+    public partial class Client : Form
+    {
+        private Socket clientSocket;
+
+        List<Phim> DanhSachPhim = new List<Phim>();
+
+        class Phim
+        {
+            public string TenPhim { get; set; }
+            public int GiaVeChuan { get; set; }
+            public int[] PhongChieu { get; set; }
+        }
+        public Client()
+        {
+            InitializeComponent();
+            comboBox1.DisplayMember = "TenPhim";
+            comboBox2.DisplayMember = "ToString";
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                clientSocket.Connect(IPAddress.Parse("127.0.0.1"), 8080);
+
+
+                Invoke(new Action(() => MessageBox.Show("Đã kết nối đến server")));
+                byte[] buffer = new byte[1024];
+                int bytesReceived = clientSocket.Receive(buffer);
+                string jsonData = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+
+                // Chuyển dữ liệu JSON sang danh sách phim
+                List<Phim> danhSachPhim = JsonConvert.DeserializeObject<List<Phim>>(jsonData);
+
+                // Hiển thị danh sách phim trong comboBox
+                comboBox1.Invoke(new Action(() =>
+                {
+                    comboBox1.DataSource = danhSachPhim;
+                    comboBox1.DisplayMember = "TenPhim";
+                }));
+
+                clientSocket.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Phim selectedPhim = (Phim)comboBox1.SelectedItem;
+            comboBox2.DataSource = selectedPhim.PhongChieu;
+        }
+
+        private void button_DatVe_Click(object sender, EventArgs e)
+        {
+            richTextBox_Ve.Visible = true;
+            richTextBox_Ve.Text = string.Empty;
+            Phim selectedPhim = (Phim)comboBox1.SelectedItem;
+            List<string> listcb = new List<string>();
+            int vevip = 0;
+            int vethuong = 0;
+            int vevot = 0;
+            long giave = 0;
+            //tạo danh sách chỗ ngồi cho từng loại vé
+            string[] VeVip = { "GheB2", "GheB3", "GheB4" };
+            string[] VeVot = { "GheA1", "GheA5", "GheB1", "GheB5", "GheC1", "GheC5" };
+            string[] VeThuong = { "GheA2", "GheA3", "GheA4", "GheC2", "GheC3", "GheC4" };
+
+            int giavechuan = selectedPhim.GiaVeChuan;
+            long giavevip = giavechuan * 2;
+            long giavevot = giavechuan / 4;
+            long giavethuong = giavechuan;
+            //Hiển thị tên, tên phim, phòng chiếu
+            richTextBox_Ve.AppendText($"Họ và tên: {textBox_HoVaTen.Text}\n");
+            richTextBox_Ve.AppendText($"Phim: {comboBox1.Text}\n");
+            richTextBox_Ve.AppendText($"Phòng chiếu: {comboBox2.Text}\n");
+            //hiển thị chỗ ngồi đã đặt
+            richTextBox_Ve.AppendText("Chỗ ngồi: \n");
+
+            foreach (System.Windows.Forms.CheckBox checkbox in groupBox.Controls)
+            {
+                if (checkbox.Checked)
+                {
+
+                    listcb.Add(checkbox.Text);
+                    if (VeVip.Contains(checkbox.Name))
+                    {
+                        vevip += 1;
+                        richTextBox_Ve.AppendText(checkbox.Name.Substring(checkbox.Name.Length - 2) + "\n");
+                    }
+
+                    if (VeVot.Contains(checkbox.Name))
+                    {
+                        vevot += 1;
+                        richTextBox_Ve.AppendText(checkbox.Name.Substring(checkbox.Name.Length - 2) + "\n");
+                    }
+
+                    if (VeThuong.Contains(checkbox.Name))
+                    {
+                        vethuong += 1;
+                        richTextBox_Ve.AppendText(checkbox.Name.Substring(checkbox.Name.Length - 2) + "\n");
+                    }
+                }
+            }
+            //tính tiền
+            giave = (vevip * giavevip) + (vethuong * giavethuong) + (vevot * giavevot);
+            richTextBox_Ve.AppendText($"Tổng tiền: {giave} đồng");
+        }
+    }
+}
---
+++ Lab3_Bai04/Server.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Lab3_Bai04
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView_Message = new System.Windows.Forms.ListView();
+            this.button_Listen = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button_DocFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView_Message
+            // 
+            this.listView_Message.HideSelection = false;
+            this.listView_Message.Location = new System.Drawing.Point(204, 277);
+            this.listView_Message.Name = "listView_Message";
+            this.listView_Message.Size = new System.Drawing.Size(367, 85);
+            this.listView_Message.TabIndex = 0;
+            this.listView_Message.UseCompatibleStateImageBehavior = false;
+            this.listView_Message.View = System.Windows.Forms.View.List;
+            // 
+            // button_Listen
+            // 
+            this.button_Listen.Location = new System.Drawing.Point(558, 133);
+            this.button_Listen.Name = "button_Listen";
+            this.button_Listen.Size = new System.Drawing.Size(93, 38);
+            this.button_Listen.TabIndex = 1;
+            this.button_Listen.Text = "Listen";
+            this.button_Listen.UseVisualStyleBackColor = true;
+            this.button_Listen.Click += new System.EventHandler(this.button_Listen_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(367, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(367, 153);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // button_DocFile
+            // 
+            this.button_DocFile.Location = new System.Drawing.Point(558, 192);
+            this.button_DocFile.Name = "button_DocFile";
+            this.button_DocFile.Size = new System.Drawing.Size(93, 36);
+            this.button_DocFile.TabIndex = 4;
+            this.button_DocFile.Text = "Đọc file";
+            this.button_DocFile.UseVisualStyleBackColor = true;
+            this.button_DocFile.Click += new System.EventHandler(this.button_DocFile_Click);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_DocFile);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button_Listen);
+            this.Controls.Add(this.listView_Message);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_Message;
+        private System.Windows.Forms.Button button_Listen;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button_DocFile;
+    }
+}
